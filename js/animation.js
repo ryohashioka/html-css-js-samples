84d4899738fb87ec431d68feb31343983bda0c85
ryohashioka <ryo.hashioka@gmail.com>
2022-09-06 21:49:29 +0900

    click アニメーションを追加

--- js/animation.js
+++ js/animation.js
@@ -1,3 +1,10 @@
+/********** button click event **********/
+const roteteBtnElement = document.getElementById('rotate_button')
+roteteBtnElement.addEventListener('click', (event) => {
+  document.getElementById('click_rotate').classList.add('rotate');
+})
+
+/********** scroll event **********/
 const scrollElement = document.getElementById('scroll_content');
 const boxElements = scrollElement.querySelectorAll('.scroll-box');
 
